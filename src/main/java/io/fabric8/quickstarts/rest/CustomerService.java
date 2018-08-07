5e36a9dd91c3c5564d4b5780587cf8bcbb424a26
Prem Sureddy <prem.sureddy@ericsson.com>
2018-08-07 08:22:14 +0800

    added ID

--- src/main/java/io/fabric8/quickstarts/rest/CustomerService.java
+++ src/main/java/io/fabric8/quickstarts/rest/CustomerService.java
@@ -143,8 +143,8 @@ public class CustomerService {
                                 Customer customer) {
         LOG.info("Invoking addCustomer, Customer name is: {}", customer.getName());
         customer.setId(++currentId);
-        LOG.info("Invoking addCustomer, Customer id is: {}", String.valueOf(currentId));
-
+      /*  LOG.info("Invoking addCustomer, Customer id is: {}", String.valueOf(currentId));
+*/
         customers.put(customer.getId(), customer);
         if (jaxrsContext.getHttpHeaders().getMediaType().getSubtype().equals("json")) {
             return Response.ok().type("application/json").entity(customer).build();

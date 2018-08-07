4dc9db0ecacc00ad6426c721cc15713f36bcbce2
Prem Sureddy <prem.sureddy@ericsson.com>
2018-08-07 08:37:19 +0800

    Removed Hostname

--- src/main/java/io/fabric8/quickstarts/rest/Customer.java
+++ src/main/java/io/fabric8/quickstarts/rest/Customer.java
@@ -49,6 +49,7 @@ public class Customer {
     public void setName(String name) {
         this.name = name;
         java.util.Random rand = new java.util.Random();
-        this.name = name + "-" + System.getenv().getOrDefault("HOSTNAME", "unknown") + "-" + rand.nextInt(50);
+        // this.name = name + "-" + System.getenv().getOrDefault("HOSTNAME", "unknown") + "-" + rand.nextInt(50);
+        this.name = name;
     }
 }
